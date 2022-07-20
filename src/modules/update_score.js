f0a8759ebac370222fba59a5881ca9efb0241856
principlebrothers <principlesbrothers@yahoo.com>
2022-07-20 22:24:25 +0000

    Add function to update the API with input from user

---
+++ src/modules/update_score.js
@@ -0,0 +1,18 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const gameId = 'yq8bW35oxhEU8iYMXlzp';
+const updateScoreUrl = `${baseUrl}games/${gameId}/scores/`;
+
+const updateScore = async (player, score) => {
+  await fetch(updateScoreUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: player,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+
+export default updateScore;
