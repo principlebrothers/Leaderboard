aaffd3a4a476a7ee9f29a65eaf03b11af88b5461
principlebrothers <principlesbrothers@yahoo.com>
2022-07-20 22:17:56 +0000

    Add event listener to form form, refresh button and remove declared ul variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+import updateScore from './modules/update_score';
+import getScores from './modules/get_scores';
+
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+const refreshBtn = document.querySelector('.refresh');
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  updateScore(name.value, score.value);
+  form.reset();
+});
+
+refreshBtn.addEventListener('click', () => {
+  document.location.reload()
+  getScores();
+});
+
+getScores();
