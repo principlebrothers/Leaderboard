ca6d5b34627818c4928f9257851ddbede69eb566
principlebrothers <principlesbrothers@yahoo.com>
2022-07-24 13:07:32 +0000

    Add api_url.js file

---
+++ src/modules/api_url.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const gameId = 'yq8bW35oxhEU8iYMXlzp';
+const updateScoreUrl = `${baseUrl}games/${gameId}/scores/`;
+
+export default updateScoreUrl;
